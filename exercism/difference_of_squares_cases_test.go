933017b4097e9e02f6820e47724d353adc0f7029
joaofsoares <joaofsoares@yahoo.com>
2026-04-26 18:50:27 +0100

    difference of squares

---
+++ exercism/difference_of_squares_cases_test.go
@@ -0,0 +1,133 @@
+package exercism
+
+type differenceSquaresCaseTest struct {
+	description string
+
+	fn func(int) int
+
+	fnName string
+
+	input int
+
+	expected int
+}
+
+var differenceSquaresTestCases = []differenceSquaresCaseTest{
+
+	{
+
+		description: "sum of squares 1",
+
+		fn: SumOfSquares,
+
+		fnName: "SumOfSquares",
+
+		input: 1,
+
+		expected: 1,
+	},
+
+	{
+
+		description: "sum of squares 5",
+
+		fn: SumOfSquares,
+
+		fnName: "SumOfSquares",
+
+		input: 5,
+
+		expected: 55,
+	},
+
+	{
+
+		description: "sum of squares 100",
+
+		fn: SumOfSquares,
+
+		fnName: "SumOfSquares",
+
+		input: 100,
+
+		expected: 338350,
+	},
+
+	{
+
+		description: "square of sum 1",
+
+		fn: SquareOfSum,
+
+		fnName: "SquareOfSum",
+
+		input: 1,
+
+		expected: 1,
+	},
+
+	{
+
+		description: "square of sum 5",
+
+		fn: SquareOfSum,
+
+		fnName: "SquareOfSum",
+
+		input: 5,
+
+		expected: 225,
+	},
+
+	{
+
+		description: "square of sum 100",
+
+		fn: SquareOfSum,
+
+		fnName: "SquareOfSum",
+
+		input: 100,
+
+		expected: 25502500,
+	},
+
+	{
+
+		description: "difference of squares 1",
+
+		fn: Difference,
+
+		fnName: "Difference",
+
+		input: 1,
+
+		expected: 0,
+	},
+
+	{
+
+		description: "difference of squares 5",
+
+		fn: Difference,
+
+		fnName: "Difference",
+
+		input: 5,
+
+		expected: 170,
+	},
+
+	{
+
+		description: "difference of squares 100",
+
+		fn: Difference,
+
+		fnName: "Difference",
+
+		input: 100,
+
+		expected: 25164150,
+	},
+}
